6e1374a871b4644e88a63fa4f79e8a97650d8af1
Enddy Dumbrique <enddynayn@hotmail.com>
2013-10-26 18:43:49 -0700

    psuedo code for launch func

---
+++ init.rb
@@ -0,0 +1,12 @@
+## Food Finder ##
+#
+# Launch this Ruby file from command line
+# to get started
+#
+
+APP_ROOT = File.dirname(__FILE__)
+
+require "#{APP_ROOT}/lib/guide" 
+
+$: .unshift(File.join(APP_ROOT, 'lib')) #look in this folder
+require 'guide'
---
+++ lib/guide.rb
@@ -0,0 +1,18 @@
+class Guide
+#controller class	
+	def initialize(path=nil)
+		# locate the restaurant text file at path
+		# or create a new file
+		# exit if create fails
+		
+	end
+
+	def launch!
+		#introduction
+		#action loop
+		# what do you want to do? (list, find, add,, quit)
+		# do that action
+		#reapeat until user quits
+		#conclusion
+
+end
