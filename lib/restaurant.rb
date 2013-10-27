d0f561bc57d4182c682ac314350f690e95aff690
Enddy Dumbrique <enddynayn@hotmail.com>
2013-10-26 18:55:27 -0700

    add restaurant class file

---
+++ lib/restaurant.rb
@@ -0,0 +1,14 @@
+class Restaurant
+
+	@@filepath = nil # class variable we dont need an instance
+
+	def self.create_file
+		# create the restaurant file
+	end
+
+	def self.save_restaurants
+		# read the restaurant file
+		# return instances of restaurant
+	end
+
+end
